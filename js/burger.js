fec0478c997f54d021d8ff2d2687621e6badc389
Xenia Filippova <32355526+riva-sm@users.noreply.github.com>
2021-11-27 00:38:16 +0800

    сделали адаптив сайта, добавили бургер меню

---
+++ js/burger.js
@@ -0,0 +1,17 @@
+const burgerMenu = (option) => {
+  const {
+    selectorBurger = ".burger",
+    activeBurger = "burger_active",
+    selectorMenu,
+    openMenuSelector,
+  } = option;
+
+  const burger = document.querySelector(selectorBurger);
+  const menu = document.querySelector(selectorMenu);
+  burger.addEventListener("click", () => {
+    burger.classList.toggle(activeBurger);
+    menu.classList.toggle(openMenuSelector);
+  });
+};
+
+export default burgerMenu;
